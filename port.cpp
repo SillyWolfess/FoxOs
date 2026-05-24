c4917c26e0996edaa1055087d58fa4d2ff6a9575
Silly Wolfess <silier.vixen@gmail.com>
2026-05-24 10:49:33 -0400

    remove logs from port

--- port.cpp
+++ port.cpp
@@ -7,10 +7,6 @@ Port::~Port() {};
 Port8Bit::Port8Bit(uint16_t number):Port(number) { }
 Port8Bit::~Port8Bit(){};
 void Port8Bit::write(uint8_t data) {
-    Terminal::s_terminal->writeHex8(data);
-    Terminal::s_terminal->writestring(" -> ");
-    Terminal::s_terminal->writeHex16(number);
-    Terminal::s_terminal->writestring("\n");
     __asm__ volatile("outb %0, %1": :"a" (data), "dN" (number));
 }
 uint8_t Port8Bit::read() {
@@ -23,10 +19,6 @@ uint8_t Port8Bit::read() {
 Port8BitSlow::Port8BitSlow(uint16_t number):Port8Bit(number) { }
 Port8BitSlow::~Port8BitSlow(){};
 void Port8BitSlow::write(uint8_t data){
-    Terminal::s_terminal->writeHex8(data);
-    Terminal::s_terminal->writestring(" -> ");
-    Terminal::s_terminal->writeHex16(number);
-    Terminal::s_terminal->writestring("\n");
     __asm__ volatile("outb %0, %1\njmp 1f\n1: jmp 1f\n1:": :"a" (data), "Nd" (number));
 }
 
