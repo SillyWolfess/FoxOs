68f93bc1cda3201cc7bbb0bd77e67990c6f8f277
Silly Wolfess <silier.vixen@gmail.com>
2026-05-25 13:45:22 -0400

    clear terminal when full

--- terminal.cpp
+++ terminal.cpp
@@ -111,17 +111,20 @@ void Terminal::putchar(char c)
     if (c == '\n') {
         ++terminal_row;
         terminal_column = 0;
-        if (terminal_row > VGA_HEIGHT) {
+        if (terminal_row >= VGA_HEIGHT) {
             terminal_row = 0;
-        //    clear();
+            clear();
         }
         return;
+    }
+    if (terminal_row >= VGA_HEIGHT) {
+        terminal_row = 0;
+        clear();
     }
     putentryat(c, terminal_color, terminal_column, terminal_row);
     if (++terminal_column == VGA_WIDTH) {
         terminal_column = 0;
-        if (++terminal_row == VGA_HEIGHT)
-            terminal_row = 0;
+        ++terminal_row;
     }
 }
 
